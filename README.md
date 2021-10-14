2967370cbc164c33d086abcc285537988c04458b
yassin-l <elhirchek@gmail.com>
2021-10-14 13:50:31 +0100

    build 0.2

--- README.md
+++ README.md
@@ -1,2 +1,76 @@
 # Tor
+A minimalist cli for stream torrent or get magnet link.
 
+## Pros
+
+- Easy to use.
+- Faster way to surf torrent.
+- Easy to set up: singel file.
+
+## 1.Installation 
+
+#### Dependencies
+- Python 3
+- Pip
+- Nuitka for compilation (optional)
+- Mpv for stream (optional)
+> Note: For window source code installation you will need MinGw64.
+#### From source code
+- Linux or Windows
+```bash
+git clone https://github.com/yassin-l/tor && cd tor
+pip install requirements.txt 
+python -m nuitka --onefile -o tor tor.py
+# this line work only for linux
+sudo ln ./tor /usr/bin/tor
+```
+#### For binary install
+- Linux or Windows
+```bash
+npm install tor-cli -g
+#or 
+yarn install tor-cli -g
+```
+## 2.Usage
+```bash
+tor -h or --help #for help
+
+Tor
+   *usage:
+   tor website options names
+   
+   *website:
+   -n --> nyaa.si
+   -t --> torrentGalaxy
+   -r --> rarbg
+   
+   *options:
+   -s --> for stream using mpv and webtorrent
+   -m --> return magnet_link
+   -d --> downlaod torrent using aria2 #still in dev
+```
+```bash
+tor -r -m breaking.bad #print resault of search
+0 ==> Breaking Bad Season 2 Complete (1080p)
+['TV/HD', '2021-09-20 11:03:34', '9.2 GB', '0\n', '0', 'eluway']
+1 ==> Breaking Bad Season 3 Complete (1080p)
+['TV/HD', '2021-09-20 11:03:34', '7.5 GB', '1\n', '0', 'eluway']
+2 ==> Breaking Bad Season 4 Complete (1080)
+['TV/HD', '2021-09-20 11:03:34', '6.8 GB', '0\n', '0', 'eluway']
+3 ==> Breaking Bad S01 2008 720p 10bit BluRay English AAC 5.1 x265 - mkvAnime [Telly]
+['TV/HEVC/x265', '2021-07-01 11:25:44', '2.4 GB', '17\n', '7', 'movieslover']
+4 ==> Breaking Bad S04 2011 720p 10bit BluRay English AAC 5.1 x265 - mkvAnime [Telly]
+['TV/HEVC/x265', '2021-07-01 11:01:03', '4.3 GB', '10\n', '6', 'movieslover']
+5 ==> Breaking Bad S05 2012 720p 10bit BluRay English AAC 5.1 x265 - mkvAnime [Telly]
+['TV/HEVC/x265', '2021-07-01 11:01:03', '5.4 GB', '9\n', '7', 'movieslover']
+6 ==> Breaking Bad S02 2009 720p 10bit BluRay English AAC 5.1 x265 - mkvAnime [Telly]
+['TV/HEVC/x265', '2021-07-01 11:01:02', '4.3 GB', '14\n', '9', 'movieslover']
+7 ==> Breaking Bad S03 2010 720p 10bit BluRay English AAC 5.1 x265 - mkvAnime [Telly]
+['TV/HEVC/x265', '2021-07-01 11:01:02', '4.3 GB', '12\n', '6', 'movieslover']
+8 ==> Breaking Bad S01-05e01-62 DVD9 - Ita Eng Ger Fra Spa Cec Pol - MultiSub 21 DVD - SERIE COMPLETA
+['TV/DVD', '2021-03-15 11:04:22', '148.5 GB', '0\n', '2', 'Plusam']
+9 ==> Breaking Bad (2008) Season 1-5 S01-S05 (1080p BluRay x265 HEVC 10bit AAC 5.1 Silence) [QxR]
+['TV/HEVC/x265', '2021-02-25 11:05:49', '140.1 GB', '58\n', '208', 'QxR']
+10 ==> El.Camino.A.Breaking.Bad.Movie.2019.1080p.WEBRip.x264-WOW
+['Movies/HD', '2020-11-14 11:03:56', '1.8 GB', '7\n', '1', 'jalim']
+```
--- tor.py
+++ tor.py
@@ -1,79 +1,127 @@
-import requests,sys,subprocess,re
+import sys,requests,subprocess
+from rich import print
 from bs4 import BeautifulSoup as bs
+
+#Website
 
 class WebSite:
     
     def __init__(self,url:str = None,name:str = None):
         
         self.url = f"{url}{name}"
-        if name != "None" or name == "-h" or name == "--help":res = requests.get(self.url).content
-        self.soup = bs(res,'lxml')
         self.names = []
-        self.magnet = []
-        self.num:str = None
+        self.info = []
+        self.num:int = None
         
-    def mag(self):
-        
-        if self.magnet == []:
-            print("set list comprehension ^_^")
-        else:
-            return self.magnet[self.num]
+    def soup_(self):
+
+        res = requests.get(f"{self.url}").content
+        self.soup = bs(res,'html.parser') 
+        return self.soup
+
+    def prin_t(self,n:int):
+
+        j =int(0)
+        for x,i in enumerate(self.names):
+            print(f"[blue]{x} ==> {i.string}[/blue]") 
+            while j != "t":
+                print(self.info[j:j+n])
+                j+=n
+                break
+    
+    def scend_soup(self,s_url):
+
+        scend_res = requests.get(f"{s_url}{self.names[self.num].get('href')}").content
+        scend_soup = bs(scend_res,'html.parser')
+        self.magnet = [i.get('href') for i in scend_soup.find_all('a',href=True) if str(i.get('href')).startswith('magnet')]
+        return self.magnet
 
     def stream(self):
-        subprocess.run(["webtorrent", "--playlist", "--mpv",f'{self.mag()}'],shell=True)
+        
+        subprocess.run([f"peerflix -a --mpv -r \"{self.magnet[0]}\""],shell=True)
     
-    def download(self):
-        subprocces.run(['aria2c',f'{self.mag()}'],shell=True)
+    # def download(self):
+    #     subprocces.run(['aria2c',f'{self.mag()}'],shell=True)
+
+#Website
 
 #help
-if sys.argv[-1] == "-h" or sys.argv[-1] == "--help":
-           print(
-           """tor [website] [options] [names]
-           [website]
-           -n --> nyaa.si
-           -t --> torrentGalaxy
-           [options]
-           -s --> for stream using mpv and webtorrent
-           -m --> return magnet_link
-           -d --> downlaod torrent using aria2
-           [example]
-           tor -n -s attack.on.titan
-           tor -t -s game.of.thrones
-           """
-           )
-           sys.exit(0)
+
+if sys.argv[-1] == "-h" or sys.argv[-1] == "--help" or len(sys.argv) == 1:
+    print(
+       """
+Tor
+    *usage:
+    tor website options names
+
+    *website:
+    -n --> nyaa.si
+    -t --> torrentGalaxy
+    -r --> rarbg
+
+    *options:
+    -s --> for stream using mpv and webtorrent
+    -m --> return magnet_link
+    -d --> downlaod torrent using aria2 //still not working
+
+    *ex:
+    tor -n -s attack.on.titan
+    tor -t -s game.of.thrones
+"""
+)
+    sys.exit()
+
 #help
-#nyaa.si
-nyaa = WebSite("https://nyaa.si/?f=0&c=0_0&q=",f"{sys.argv[-1]}")
-nyaa.names = [i.text for i in nyaa.soup.find_all("a",title=True) if not str(i.text).startswith('\n')]
-nyaa.magnet = [i.get('href') for i in nyaa.soup.find_all('a',href=True) if str(i.get('href')).startswith('mag')]
 
-if "-n" in sys.argv and "-m" in sys.argv:
-    for x,i in enumerate(nyaa.names):print(x,"-->",i)
-    num = int(input("enter num: "))
-    nyaa.num = num
-    print(nyaa.mag())
+#rarbg
 
-if "-n" in sys.argv and "-s" in sys.argv:
-    for x,i in enumerate(nyaa.names):print(x,"-->",i)
-    num = int(input("enter num: "))
-    nyaa.num = num
-    nyaa.stream()
+if "-r" in sys.argv:
+
+    rarbg = WebSite("https://rarbg.unblockninja.com/search/?search=",f"{sys.argv[-1]}")
+    rarbg.soup_()
+    rarbg.names = [i for i in rarbg.soup.find_all("a",href=True,title=True) if not str(i.text).startswith("\n")]
+    rarbg.info = [i.text for i in rarbg.soup.find_all('td',class_='lista') if not str(i.text).startswith('\n')]
+    rarbg.prin_t(6)
+    rarbg.num = int(input("enter the number:")) 
+    rarbg.scend_soup("https://rarbg.unblockninja.com")
+    if "-m" in sys.argv:
+        print(f"[blue]{rarbg.magnet[0]}[/blue]")
+    elif "-s" in sys.argv:
+        rarbg.stream()
+
+#rarbg
 #nyaa.si
+
+if "-n" in sys.argv:
+
+    nyaa = WebSite("https://nyaa.si/?f=0&c=0_0&q=",f"{sys.argv[-1]}")
+    nyaa.soup_()
+    nyaa.names = [i for i in nyaa.soup.find_all("a",title=True) if not str(i.text).startswith('\n')] 
+    nyaa.info = [i.text for i in nyaa.soup.find_all('td',class_='text-center') if not str(i.text).startswith('\n')]
+    nyaa.prin_t(5)
+    nyaa.num = int(input("enter the number:"))
+    nyaa.scend_soup("https://nyaa.si")
+    if "-m" in sys.argv:
+        print(f"[blue]{nyaa.magnet[0]}[/blue]")
+    elif "-s" in sys.argv:
+        nyaa.stream()
+
+#nyaa.si
+
 #torrentGalaxy
-torrent_galaxy = WebSite("https://torrentgalaxy.to/torrents.php?search=",f"{sys.argv[-1]}")
-torrent_galaxy.names = [i.text for i in torrent_galaxy.soup.find_all('a',title=True,class_='txlight') if not re.match(r'\d',str(i.text))]
-torrent_galaxy.magnet = [i.get('href') for i in torrent_galaxy.soup.find_all('a',href=True) if str(i.get('href')).startswith('magnet')]
 
-if "-t" in sys.argv and "-m" in sys.argv:
-    for x,i in enumerate(torrent_galaxy.names):print(x,"-->",i)
-    num = int(input("enter num: "))
-    torrent_galaxy.num = num
-    print(torrent_galaxy.mag())
-
-if "-t" in sys.argv and "-s" in sys.argv:
-    for x,i in enumerate(torrent_galaxy.names):print(x,"-->",i)
-    num = int(input("enter num: "))
-    torrent_galaxy.num = num
-    torrent_galaxy.stream()
+if "-t" in sys.argv:
+    
+    torrent_galaxy = WebSite("https://torrentgalaxy.to/torrents.php?search=",f"{sys.argv[-1]}")
+    torrent_galaxy.soup_()
+    torrent_galaxy.names = [i for i in torrent_galaxy.soup.find_all('a',title=True,class_='txlight') if not re.match(r'\d',str(i.text))]
+    torrent_galaxy.info = [i.text for i in torrent_galaxy.soup.find_all('div',class_="tgxtablecell collapsehide rounded txlight") if not str(i.text).startswith('\n') and str(i.text) != '-' and str(i.text) != '']
+    torrent_galaxy.prin_t(5)
+    torrent_galaxy.num = int(input("enter the number:"))
+    torrent_galaxy.scend_soup("https://torrentgalaxy.to")
+    if "-m" in sys.argv:
+        print(f"[blue]{torrent_galaxy.magnet[0]}[/blue]")
+    elif "-s" in sys.argv:
+        torrent_galaxy.stream()
+    
 #torrentGalaxy
